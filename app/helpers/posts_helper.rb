93e31b266a80aa010123e7be0836dfd7d780386e
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-05-30 17:57:24 +0000

    Automatically backed up by Learn

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,3 +1,9 @@
 module PostsHelper
-  
+  def author_select
+    if @post.author.nil?
+      f.select :author_id, options_from_collection_for_select(Author.all, :id, :name)<br>
+    else
+      f.hidden_field :author_id
+    end
+  end
 end

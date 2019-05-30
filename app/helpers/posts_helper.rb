a0ed4706b4b6a0b893df26af44d13062fdfaf0bd
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-05-30 17:57:41 +0000

    Automatically backed up by Learn

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,7 @@
 module PostsHelper
   def author_select
     if @post.author.nil?
-      f.select :author_id, options_from_collection_for_select(Author.all, :id, :name)<br>
+      f.select :author_id, options_from_collection_for_select(Author.all, :id, :name)
     else
       f.hidden_field :author_id
     end
